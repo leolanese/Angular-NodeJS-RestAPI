170d0c5bfb6f5b7099b663a3d8ddc05d4dea44db
leolanese <laneseleo@gmail.com>
2024-02-09 11:14:10 +0000

    Angular +  NodeJS: adding provider

--- users-rest-api/src/app/app.component.ts
+++ users-rest-api/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
