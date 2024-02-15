0e58ffc8a7599f12d13f5d76613b2b121008d9e6
Leo Lanese <laneseleo@gmail.com>
2024-02-15 13:58:59 +0000

    Update api-user.service.spec.ts

--- users-rest-api/src/app/services/api-user.service.spec.ts
+++ users-rest-api/src/app/services/api-user.service.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { APIUserService } from './api-user.service';
 
 describe('APIUserService', () => {
   let service: APIUserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(APIUserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('getUsersRestApiNodeJS', () => {
+    it('should return expected users', () => {
+      const expectedUsers = [
+        {id: 1, name: 'User 1'},
+        {id: 2, name: 'User 2'}
+      ];
+
+      service.getUsersRestApiNodeJS().subscribe(
+        users => expect(users).toEqual(expectedUsers)
+      );
+
+      const req = httpTestingController.expectOne(service.restApiNodeJS);
+      expect(req.request.method).toEqual('GET');
+      req.flush(expectedUsers);
+    });
+
+    it('should return empty array on error', () => {
+      const errorResponse = new ErrorEvent('Network error');
+
+      service.getUsersRestApiNodeJS().subscribe(
+        users => expect(users.length).toEqual(0)
+      );
+
+      const req = httpTestingController.expectOne(service.restApiNodeJS);
+      req.error(errorResponse);
+    });
   });
 });
